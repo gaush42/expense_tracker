b93c9e800896ea582eacbe37421de31ca2b7f145
Hachiman Hikigaya <48960647+gaush42@users.noreply.github.com>
2025-04-22 14:35:51 +0530

    password hashing

--- controller/signUp.js
+++ controller/signUp.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const bcrypt = require('bcryptjs')
 
 const User = require('../model/userModel')
 
@@ -16,7 +16,8 @@ exports.RegisterUser = async (req, res) => {
                 message: 'Email already exists.'
             })
         }
-        const newUser = await User.create({ fullname, email, password})
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const newUser = await User.create({ fullname, email, password:hashedPassword})
         res.status(201).json({
             message: 'User registered successfully.', user: newUser
         })
@@ -32,10 +33,12 @@ exports.Login = async (req, res) => {
             return res.status(400).json({message:"All fields are required."})
         }
         const user = await User.findOne({where: {email}})
-        if(!user || user.password !== password){
+        const isPasswordMatched = await bcrypt.compare(password, user.password)
+
+        if(!user || !isPasswordMatched){
             return res.status(401).json({message: 'Invalid email or Password'})
         }
-        res.status(200).json({ message: 'Login successful.', user: { id: user.id, fullname: user.fullname, email: user.email } });
+        res.status(200).json({ message: 'Login successful.'});
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: 'Server error.' });

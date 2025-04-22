a5d71e189bcc2c3e5e2beaec2b765721b0cc9aa1
Hachiman Hikigaya <48960647+gaush42@users.noreply.github.com>
2025-04-22 11:49:40 +0530

    login backend api

--- controller/signUp.js
+++ controller/signUp.js
@@ -25,3 +25,19 @@ exports.RegisterUser = async (req, res) => {
         res.status(500).json({message: 'Server error.'})
     }
 }
+exports.Login = async (req, res) => {
+    try{
+        const { email, password } = req.body
+        if(!email || !password){
+            return res.status(400).json({message:"All fields are required."})
+        }
+        const user = await User.findOne({where: {email}})
+        if(!user || user.password !== password){
+            return res.status(401).json({message: 'Invalid email or Password'})
+        }
+        res.status(200).json({ message: 'Login successful.', user: { id: user.id, fullname: user.fullname, email: user.email } });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error.' });
+    }
+}
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const signUpController = require('../controller/signUp')
 
 router.post("/signup", signUpController.RegisterUser)
+router.post("/login", signUpController.Login)
 
 module.exports = router
